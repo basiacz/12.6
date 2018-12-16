19795c07afeece4210ddd92e730d5ab18b4f5df4
basiacz <basiaczerw@gmail.com>
2018-12-16 18:54:55 +0100

    12.6

--- js/script.js
+++ js/script.js
@@ -6,7 +6,9 @@ document.getElementById('search').addEventListener('click', searchCountries);
 function searchCountries() {
   var countryName = document.getElementById('country-name').value;
 
-  if (!countryName.length) { countryName = 'Poland' };
+  if (!countryName.length) { 
+  countryName = 'Poland';
+  }
 
   fetch(url + countryName)
     .then(function(resp) {
